6c0a0713604009b786508cbd51c8dccdf6527a9f
samay1012 <202001070@daiict.ac.in>
2023-04-24 21:45:44 +0530

    Admin Navbar

--- AMS/frontend/src/App.js
+++ AMS/frontend/src/App.js
@@ -3,6 +3,8 @@ import Login from './Login/Login';
 import Student from './Login/Student/Student'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Faculty from './Login/Faculty/Faculty';
+import Admin from './Login/Admin/StudentAttendance';
+
 function App() {
   return (
     <>
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<Login/>}/>
         <Route path="/Student/*" element={<Student/>}/>
         <Route path="/Faculty/*" element={<Faculty/>}/> 
+        <Route path="/Admin/*" element={<Admin/>}/>
       </Routes>
     </BrowserRouter>
 
