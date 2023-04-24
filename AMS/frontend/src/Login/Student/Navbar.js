bcc1a5e6c794fe786ab899979f29530b5908ffef
Sahil Kothari <kotharisahil4@gmail.com>
2023-04-24 19:39:10 +0530

    Improved Student navbar and profile

--- AMS/frontend/src/Login/Student/Navbar.js
+++ AMS/frontend/src/Login/Student/Navbar.js
@@ -4,9 +4,9 @@ import './Navbar.css'
 function Navbar(){
     return (   
       
-          <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{backgroundColor: "blue"}}>
+          <nav className="navbar navbar-expand-lg"  style={{backgroundColor: '#40513B', }}>
           <div className="container-fluid" >
-             <Link className="navbar-brand" to="/Student/Profile">Profile</Link>
+             <Link className="navbar-brand" to="/Student/Profile" style={{color: 'white'}}>Profile</Link>
 
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
@@ -16,16 +16,16 @@ function Navbar(){
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
                 
-                  <Link className="nav-link active" to="/Student/SeeCourses">See all courses</Link>
+                  <Link className="nav-link active" to="/Student/SeeCourses" style={{color: 'white'}}>See all courses</Link>
         
 
                 <li className="nav-item">
-                  <Link className="nav-link active" to="/Student/MedicalForm">Apply for medical leave</Link>
+                  <Link className="nav-link active" to="../Leave/MedicalForm" style={{color: 'white'}}>Apply for Leave</Link>
                 </li>
                 
               </ul>
 
-              <Link className="nav-link active" to="/">Logout</Link>
+              <Link className="nav-link active" to="/"  style={{ color: 'white'}}>Logout</Link>
             </div>
           </div>
         </nav>
