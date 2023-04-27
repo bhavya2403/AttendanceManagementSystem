0be89265248bd22e16baec82ac181b569a6eb76d
Sahil Kothari <kotharisahil4@gmail.com>
2023-04-27 11:00:08 +0530

    commit

--- AMS/frontend/src/Login/Student/Profile.js
+++ AMS/frontend/src/Login/Student/Profile.js
@@ -7,13 +7,10 @@ import { SelectProvider } from '@mui/base';
 
 
 function Profile(props) {
-<<<<<<< HEAD
-  const [data, setData] = useState(null); // set initial state to null
   const [isLoading, setIsLoading] = useState(true); // add loading state
-=======
+
   // declaring the states
   const [data, setData] = useState("");
->>>>>>> 71f8d8a12a74a86a4f5a91c7183c704af382458e
 
   const location = useLocation();
   const token = location?.state?.token;
@@ -29,7 +26,6 @@ function Profile(props) {
 
   async function sendingReq() {
     try {
-<<<<<<< HEAD
       const response = await fetch("/student/", requestOptions);
       const data_local = await response.json();
       setData(data_local);
@@ -62,39 +58,11 @@ function Profile(props) {
       )}
     </>
   );
-=======
+      }
+export default Profile
 
-      console.log("before calling");
-      const response = await fetch("/student/", requestOptions);
-      console.log("after calling");
-      data_local = await response.json();
-      setData(data_local);
-      console.log(data);
-      console.log("after function");
-    } catch (err) {
-    }
-  }
-  sendingReq()
-  return (
-    <>
-      <p>{data.toString()} </p>
-      <div style={{ display: 'flex' }}>
-        <div style={{ display: 'inline-block', marginRight: '20px' }}>
-          <ProfileCard sData={data} />
-        </div>
-        <div style={{ display: 'inline-block' }}>
-          <ProgressBar sData={data} />
-        </div>
-      </div>
-    </>
-  )
-  // } 
-  // else {
-  //   return <div>loadoing</div>;
-  // }
->>>>>>> 71f8d8a12a74a86a4f5a91c7183c704af382458e
-}
+   
 
 
 
-export default Profile;
+
