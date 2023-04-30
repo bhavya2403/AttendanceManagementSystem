105508ad43c3225fc2e426ed5c9f8aa484c143f5
vineet191202 <202001060@daiict.ac.in>
2023-04-30 15:39:55 +0530

    user register changes

--- AMS/frontend/src/Login/Admin/RegisterCourse.js
+++ AMS/frontend/src/Login/Admin/RegisterCourse.js
@@ -6,40 +6,78 @@ function CourseRegistrationForm() {
   const [Courseinstructor, setCoureInstructor] = useState("");
   const [courseID, setCourseID] = useState("");
   const [department, setDepartment] = useState("");
-  const [coursedescription, setCoursedescription] = useState("");
+  const [semester, setsemester] = useState("");
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(`Course: ${courseName}\nCourseInstructor: ${Courseinstructor}\ncourseID: ${courseID}\nCoursedescription: ${coursedescription}`);
+    // formData.append('file', file);
+
+    try {
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'token': `${window.token}`,
+          'Content-Type': 'application/json',
+          
+        },
+        body: JSON.stringify({
+          
+          // 'file': file,
+            'code': courseID,
+            'semester': semester,
+            'name': courseName,
+            'department': department,
+            'instructor': Courseinstructor,
+            'description': "",  
+
+        }),
+      }
+      
+      const response = await fetch("admin/register_course/", requestOptions);
+      const data = await response.json();
+      if (response.ok) {
+          console.log(response);
+      }
+      console.log(response);
+    } catch (error) {
+
+    }
+
+    // console.log(`Course: ${courseName}\nCourseInstructor: ${Courseinstructor}\ncourseID: ${courseID}\nsemester: ${semester}`);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Course Name:
-        <input type="text" value={courseName} onChange={(e) => setCourseName(e.target.value)} />
+        <input id="cname" type="text" value={courseName} onChange={(e) => setCourseName(e.target.value)} />
       </label>
       <br />
       <label>
         Course Instructor:
-        <input type="text" value={Courseinstructor} onChange={(e) => setCoureInstructor(e.target.value)} />
+        <input id="cinstruct" type="text" value={Courseinstructor} onChange={(e) => setCoureInstructor(e.target.value)} />
       </label>
       <br />
       <label>
         Course ID:
-        <input type="courseID" value={courseID} onChange={(e) => setCourseID(e.target.value)} />
+        <input id="cid" type="courseID" value={courseID} onChange={(e) => setCourseID(e.target.value)} />
       </label>
       <br />
       <label>
         Department:
-        <input type="department" value={department} onChange={(e) => setDepartment(e.target.value)} />
+        <input id="dep" type="department" value={department} onChange={(e) => setDepartment(e.target.value)} />
       </label>
       <br />
       <label>
-        Course descriptiom:
-        <textarea value={coursedescription} className="description" onChange={(e) => setCoursedescription(e.target.value)} placeholder="Course description"></textarea>
+        Semester:
+        <input id="dep" type="department" value={semester} onChange={(e) => setsemester(e.target.value)} />
       </label>
       <br />
+      {/* <label>
+        Semester:
+        <textarea id="desc" value={semester} className="description" onChange={(e) => setsemester(e.target.value)} placeholder="Semester"></textarea>
+      </label>
+      <br /> */}
       <button type="submit">Register</button>
     </form>
   );
